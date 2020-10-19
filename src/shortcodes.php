5a74c2455a8b391ad6bd3a634f78c9163bcb2d06
jimcutler <63164721+jimcutler@users.noreply.github.com>
2020-10-19 11:40:08 -0700

    Multiple Forms: Prework
    Allow a different Form ID to be specified when rendering forms and fields.
    Add nonces as needed.

--- src/shortcodes.php
+++ src/shortcodes.php
@@ -1,12 +1,15 @@
 <?php
-function wpspf_paymentform(){
+function wpspf_paymentform($atts = []){
+    extract(shortcode_atts(array(
+        'form_id' => 1,
+    ), $atts));
     $publickey = get_option( 'wpspf_sitekey' );
     $admin_ajax_url = admin_url('admin-ajax.php');
     
     $formHtml = '<div class="payment_box payment_method_authorizenet_lightweight"><h1>'.esc_attr( get_option('wpspf_paymentheading') ).'</h1><div class="wpspf_form_container" id="wpspf_form_container">
         <form method="post" id="wpspf_form" onsubmit="return wpspfCheckGrecaptcha();" name="payment" enctype="multipart/form-data" action="">            
         <table id="wc-authorizenet_lightweight-cc-form" class="wc-credit-card-form wc-payment-form">';
-    $formFields = wpspf_get_form_fields();
+    $formFields = wpspf_get_form_fields($form_id);
         //override the form fields
         $formFields = apply_filters( 'wpspf_frontend_form_fields', $formFields);
     if(!empty($formFields) && count($formFields)>0){
